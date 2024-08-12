18e8cafa42c7f55443eb8b18c560cffe8fcdf3fd
Dibbo Chowdhury <66284130+RudraShivm@users.noreply.github.com>
2024-08-13 04:26:49 +0600

    13/08/24

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -47,7 +47,7 @@ if (window.location.pathname !== "/auth") {
 
 const handleApiCall = async (apiCall) => {
   const response = await apiCall();
-  if(response.data && response.access_token){
+  if(response.data && response.data.access_token){
     const { data } = response;
     const { access_token } = data;
     if (access_token) {
